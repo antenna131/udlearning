dd3f747507a0928c3d57b6b5993e8e9c61a0ee41
Anna <anna@tolstouhhov.com>
2022-09-19 14:12:35 +0300

    create style.css

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use script";
+
+const storeName = "Rocca";
+const storeDescription = {
+    employees: ['Katya','Olga','Anna'],
+    products: {
+        dress: 100,
+        shirt:150
+    },
+    open: true
+};
+console.log(storeDescription.employees);
+
+// alert ('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18 лет?", "18");
+// console.log(answer); 
