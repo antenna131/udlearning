4cd1f44d5787fa413b1d655c500f610c4836163d
Anna <anna@tolstouhhov.com>
2023-05-02 11:50:58 +0300

    third task

--- js/script.js
+++ js/script.js
@@ -1,20 +1,117 @@
-"use script";
+/* Задание на урок:
 
-const storeName = "Rocca";
-const storeDescription = {
-    employees: ['Katya','Olga','Anna'],
-    products: {
-        dress: 100,
-        shirt:150
-    },
-    open: true
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+ 
+
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict';
+
+let numberOfFilms;
+/* Превратим в функцию запрос от пользователя и автоматизируем, когда нужно спросить еще раз = если строка, Отменить или не число;
+*/
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+
+ 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-console.log(storeDescription.employees);
+/*
+Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+*/
+function showMyDB(hidden) {
+    if (!hidden){
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
 
-// alert ('Hello');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-// const result = confirm("Are you here?");
-// console.log(result);
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-// const answer = prompt("Вам есть 18 лет?", "18");
-// console.log(answer); 
+// console.log(personalMovieDB);
+
+// 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+function rememberMyFilms() {
+    for (let i=0;i<2;i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+// rememberMyFilms();
+
+function detectPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+// detectPersonalLevel();
+
+/*
+Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+*/
+function writeYourGenres() {
+    for(let i=1; i<=3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
+
+
+// 2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+//     отменить ответ или ввести название фильма длинее, чем 50 символов.Если это происходит -
+//         возвращаем пользователя к вопросам опять
+
+// Методы и свойства строк и чисел
+// const str = 'Hello, Anna and friends!';
+
+// console.log(str.slice(12, 16));
